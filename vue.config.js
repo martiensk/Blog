b147c81c48af9631457172f2c7f506b08a0f01bc
Martiens Kropff <martiensk@gmail.com>
2020-08-04 14:36:51 +0100

    Modifying SSR to work with heroku

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     transpileDependencies: ['vuetify'],
     pluginOptions: {
         ssr: {
+            port: 80,
             entry: (target) => `./src/entry-${target}`,
             favicon: './public/favicon.ico',
             nodeExternalsWhitelist: [
